22b8a4571d12dc96859cabe4db3fe7ad72317420
woodsaj <anthony@monkey.id.au>
2016-08-12 17:39:59 +0800

    refactor demo server to act as a simple proxy for connected nodes.
    The server exposes a /nodes url to list connected nodes.
    Snapd REST calls can be made through the proxy by simply adding
    ?proxy_node=<node>  query param to every request.

--- mgmt/rest/ws_client/server/server.go
+++ mgmt/rest/ws_client/server/server.go
@@ -7,17 +7,63 @@ import (
 	"golang.org/x/net/websocket"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/rpc"
 	"net/rpc/jsonrpc"
+	"sync"
 	"time"
 
 	"github.com/codeskyblue/go-uuid"
+	"github.com/gorilla/mux"
 	"github.com/intelsdi-x/snap/mgmt/rest/client"
-	"github.com/intelsdi-x/snap/mgmt/rest/rbody"
 	"github.com/intelsdi-x/snap/mgmt/rest/ws_client"
+	"github.com/urfave/negroni"
 )
 
-var sessions map[string]*Session
+type Session struct {
+	Id         string
+	Name       string
+	socket     *websocket.Conn
+	snapClient *client.Client
+	jsonrpcCli *rpc.Client
+}
+
+type SessionStore struct {
+	sessions map[string]*Session
+	sync.Mutex
+}
+
+func (s *SessionStore) Get(id string) *Session {
+	s.Lock()
+	defer s.Unlock()
+	return s.sessions[id]
+}
+
+func (s *SessionStore) Put(id string, sess *Session) {
+	s.Lock()
+	s.sessions[id] = sess
+	s.Unlock()
+}
+
+func (s *SessionStore) Delete(id string) {
+	s.Lock()
+	delete(s.sessions, id)
+	s.Unlock()
+}
+
+func (s *SessionStore) All() []*Session {
+	s.Lock()
+	currentSessions := make([]*Session, len(s.sessions))
+	i := 0
+	for _, sess := range s.sessions {
+		currentSessions[i] = sess
+		i++
+	}
+	s.Unlock()
+	return currentSessions
+}
+
+var sessionsStore *SessionStore
 
 type wsRoundTripper struct {
 	c *rpc.Client
@@ -37,28 +83,61 @@ func (r *wsRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 }
 
 func main() {
-	sessions = make(map[string]*Session)
-	http.Handle("/ws", websocket.Handler(serveWs))
-	http.HandleFunc("/catalog", servePlugins)
-	http.ListenAndServe("localhost:7000", nil)
-
+	sessionsStore = &SessionStore{sessions: make(map[string]*Session)}
+	rtr := mux.NewRouter()
+	http.Handle("/", rtr)
+	rtr.Handle("/ws", websocket.Handler(websocketHandler))
+	rtr.PathPrefix("/v1/").HandlerFunc(proxyHandler)
+	rtr.HandleFunc("/nodes", nodesHandler)
+	n := negroni.Classic() // Includes some default middlewares
+	n.UseHandler(rtr)
+	n.Run(":7000")
 }
 
-func servePlugins(w http.ResponseWriter, r *http.Request) {
-	catalog := make([]*rbody.Metric, 0)
-	for id, sess := range sessions {
-		log.Printf("getting plugins from socket with sessionId: %s", id)
-		resp := sess.snapClient.GetMetricCatalog()
-		catalog = append(catalog, resp.Catalog...)
+func nodesHandler(w http.ResponseWriter, r *http.Request) {
+	nodes := make([]string, 0)
+	for _, sess := range sessionsStore.All() {
+		nodes = append(nodes, sess.Name)
 	}
-	body, err := json.Marshal(catalog)
+	body, err := json.Marshal(nodes)
 	if err != nil {
 		panic(err)
 	}
 	w.Write(body)
 }
 
-func serveWs(ws *websocket.Conn) {
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("handling proxy request")
+	query := r.URL.Query()
+	node := query.Get("proxy_node")
+
+	if node == "" {
+		w.WriteHeader(400)
+		w.Write([]byte("no proxy_node url param present."))
+		return
+	}
+
+	session := sessionsStore.Get(node)
+	if session == nil {
+		w.WriteHeader(404)
+		w.Write([]byte("Not found"))
+		return
+	}
+	// modify the URL PATH to strip out the request to the node.
+	director := func(req *http.Request) {
+		query := req.URL.Query()
+		query.Del("proxy_node")
+		req.URL.RawQuery = query.Encode()
+	}
+
+	proxy := httputil.ReverseProxy{
+		Director:  director,
+		Transport: &wsRoundTripper{c: session.jsonrpcCli},
+	}
+	proxy.ServeHTTP(w, r)
+}
+
+func websocketHandler(ws *websocket.Conn) {
 	log.Printf("Handler starting")
 	c := jsonrpc.NewClient(ws)
 	hc := &http.Client{
@@ -74,21 +153,24 @@ func serveWs(ws *websocket.Conn) {
 		snapClient: snapClient,
 		jsonrpcCli: c,
 	}
-	sessions[session.Id] = session
+
+	meta := make(map[string]string)
+	err = c.Call("SnapClientOverWebsocket.Metadata", session.Id, &meta)
+	if err != nil {
+		log.Printf("SnapClientOverWebsocket.Metadata error:%s", err)
+		ws.Close()
+		return
+	}
+	session.Name = meta["name"]
+	sessionsStore.Put(session.Name, session)
+	defer sessionsStore.Delete(session.Name)
 	session.Run()
 	log.Printf("Handler exiting")
-	delete(sessions, session.Id)
-}
 
-type Session struct {
-	Id         string
-	socket     *websocket.Conn
-	snapClient *client.Client
-	jsonrpcCli *rpc.Client
 }
 
 func (s *Session) Run() {
-
+	defer s.socket.Close()
 	done := make(chan struct{})
 	go func(done chan struct{}) {
 		ticker := time.NewTicker(time.Second)
@@ -102,26 +184,9 @@ func (s *Session) Run() {
 				ticker.Stop()
 				return
 			}
-			log.Printf("Heartbeat took %s", recv.Sub(sent))
+			log.Printf("Heartbeat to node %s took %s", s.Name, recv.Sub(sent))
 		}
 		close(done)
-	}(done)
-
-	go func(done chan struct{}) {
-		ticker := time.NewTicker(time.Second * 5)
-		for {
-			select {
-			case <-ticker.C:
-				resp := s.snapClient.GetPlugins(true)
-				for _, plugin := range resp.LoadedPlugins {
-					log.Printf("found plugin %s", plugin.Name)
-				}
-			case <-done:
-				log.Printf("stopping plugin list")
-				ticker.Stop()
-				return
-			}
-		}
 	}(done)
 
 	<-done
